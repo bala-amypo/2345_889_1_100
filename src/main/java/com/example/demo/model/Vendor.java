9cec6d9b5fbed64b3964a2b5c4b12b207fea13b9
bala-amypo <bala@amypo.in>
2025-12-16 10:22:07 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -0,0 +1,10 @@
+public class Vendor{
+    private Long id;
+    @vendorName
+    private String vendorName;
+    private String email;
+    private String phone;
+    private String industry;
+    private String createdAt;
+    
+}
