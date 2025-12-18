b22d7687c4d29954663b25902dc2dcc6b224c3a5
bala-amypo <bala@amypo.in>
2025-12-18 14:30:01 +0000

    User commit

--- src/main/java/com/example/demo/service/VendorService.java
+++ src/main/java/com/example/demo/service/VendorService.java
@@ -4,9 +4,8 @@ import com.example.demo.model.Vendor;
 import java.util.List;
 
 public interface VendorService {
-    Vendor create(Vendor vendor);
-    Vendor getById(Long id);
-    List<Vendor> getAll();
-    Vendor update(Long id, Vendor vendor);
-    void delete(Long id);
+    Vendor createVendor(Vendor vendor);
+    Vendor getVendor(Long id);
+    List<Vendor> getAllVendors();
+    
 }
