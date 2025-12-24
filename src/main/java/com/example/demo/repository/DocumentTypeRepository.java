3a9896f6a3b097af3a397a930ec1cdc8c3af0f55
bala-amypo <bala@amypo.in>
2025-12-24 13:54:40 +0000

    User commit

--- src/main/java/com/example/demo/repository/DocumentTypeRepository.java
+++ src/main/java/com/example/demo/repository/DocumentTypeRepository.java
@@ -1,11 +1,19 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.DocumentType;
+import com.example.demo.model.Vendor;
+import com.example.demo.model.VendorDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
-public interface DocumentTypeRepository extends JpaRepository<DocumentType, Long> {
+public interface VendorDocumentRepository
+        extends JpaRepository<VendorDocument, Long> {
 
-    List<DocumentType> findByRequiredTrue();
+    List<VendorDocument> findByVendor(Vendor vendor);
+
+    @Query("SELECT v FROM VendorDocument v WHERE v.expiryDate < :date")
+    List<VendorDocument> findExpiredDocuments(@Param("date") LocalDate date);
 }
