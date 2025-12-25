252c3a08c614ffd56a8eadcf780c163124239b58
bala-amypo <bala@amypo.in>
2025-12-25 09:16:33 +0000

    User commit

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
                 "/swagger-ui/**",
                 "/v3/api-docs/**",
                 "/swagger-ui.html",
-                "/api/vendors/**"
+                "/api/**"
         ).permitAll()
         .anyRequest().authenticated()
 )
