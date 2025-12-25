dbd79188d11af4579ed200866688629a4f1cb8ae
bala-amypo <bala@amypo.in>
2025-12-25 09:00:45 +0000

    User commit

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
         .requestMatchers(
                 "/auth/**",
+                "/api/users",
                 "/swagger-ui/**",
                 "/v3/api-docs/**",
                 "/swagger-ui.html",
